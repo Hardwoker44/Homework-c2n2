c6cd69687a22076ff46d3b1fe0dd9e01344ba796
hardw <hardwoker2017@yandex.ru>
2024-03-25 00:08:54 +0500

    2 курс Домашняя работа №2

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,11 @@ public class Main {
               people [11] = new Slytherin("Gregory","Goyle",79,91,"Slytherin",83,90,87,91,91);
         printPeople(people);
         compareTwoStudentsGryffindorAndWriteTheResult(calculateGryffindorStudentPoints1(people),calculateGryffindorStudentPoints2(people));
+        compareGriffindorStudents(people[1],people[2]);
+        compareSlytherinStudents(people[11],people[9]);
+        compareRavenclawStudents(people[4],people[5]);
+        compareHufflepuffStudents(people[7],people[8]);
+        compareHogwartsStudents(people[2],people[10]);
 
     }
     public static void printPeople(Hogwarts [] people){
@@ -28,7 +33,8 @@ public class Main {
         int sumOfFacultyProperties1 = 0;
         for(Hogwarts allPeople : people){
             if (Objects.equals(allPeople.getFaculty(), "Gryffindor") && Objects.equals(allPeople.getSurname(), "Potter")) {
-                sumOfFacultyProperties1 = allPeople.getDistanceOfTransgression() + allPeople.getPowerOfMagic();
+                sumOfFacultyProperties1 = ((Gryffindor) allPeople).getNobility() + ((Gryffindor) allPeople).getHonor() + ((Gryffindor) allPeople).getCourage();
+
             }
         }
         return sumOfFacultyProperties1;
@@ -37,7 +43,7 @@ public class Main {
         int sumOfFacultyProperties2 = 0;
         for(Hogwarts allPeople : people){
             if (Objects.equals(allPeople.getFaculty(), "Gryffindor") && Objects.equals(allPeople.getSurname(), "Granger")){
-                sumOfFacultyProperties2 = allPeople.getDistanceOfTransgression() + allPeople.getPowerOfMagic();
+                sumOfFacultyProperties2 = ((Gryffindor) allPeople).getNobility() + ((Gryffindor) allPeople).getHonor() + ((Gryffindor) allPeople).getCourage();
 
             }   // Не могу вывести getNobility и тому подобное, понимаю что нужно обратиться к дочернему классу но не могу понять как
         }
@@ -56,5 +62,66 @@ public class Main {
             System.out.println("Студенты равны");
         }
     }
+    public static void compareGriffindorStudents(Hogwarts first, Hogwarts second) {
+        int firstSum = ((Gryffindor) first).getNobility() + ((Gryffindor) first).getHonor() + ((Gryffindor) first).getCourage();
+        int secondSum = ((Gryffindor) second).getNobility() + ((Gryffindor) second).getHonor() + ((Gryffindor) second).getCourage();
+
+        if (firstSum > secondSum) {
+            System.out.println(first.getName() + " is a better Griffindor than " + second.getName());
+        } else if (secondSum > firstSum) {
+            System.out.println(second.getName() + " is better Griffindor than " + first.getName());
+        } else {
+            System.out.println(first.getName() + " and " + second.getName() + " are equals as Griffindors");
+        }
+    }
+    public static void compareSlytherinStudents(Hogwarts first, Hogwarts second) {
+        int firstSum = ((Slytherin) first).getAmbitiousness() + ((Slytherin) first).getCunning() + ((Slytherin) first).getDetermination() + ((Slytherin) first).getResourcefulness() + ((Slytherin) first).getThirstForPower();
+        int secondSum = ((Slytherin) second).getAmbitiousness() + ((Slytherin) second).getCunning() + ((Slytherin) second).getDetermination() + ((Slytherin) second).getResourcefulness() + ((Slytherin) second).getThirstForPower();
+
+        if (firstSum > secondSum) {
+            System.out.println(first.getName() + " is a better Slytherin than " + second.getName());
+        } else if (secondSum > firstSum) {
+            System.out.println(second.getName() + " is better Slytherin than " + first.getName());
+        } else {
+            System.out.println(first.getName() + " and " + second.getName() + " are equals as Slytherin");
+        }
+    }
+    public static void compareHufflepuffStudents(Hogwarts first, Hogwarts second) {
+        int firstSum = ((Hufflepuff) first).getHardworking() + ((Hufflepuff) first).getHonest() + ((Hufflepuff) first).getLoyalty();
+        int secondSum = ((Hufflepuff) second).getHardworking() + ((Hufflepuff) second).getHonest() + ((Hufflepuff) second).getLoyalty();
+
+        if (firstSum > secondSum) {
+            System.out.println(first.getName() + " is a better Hufflepuff than " + second.getName());
+        } else if (secondSum > firstSum) {
+            System.out.println(second.getName() + " is better Hufflepuff than " + first.getName());
+        } else {
+            System.out.println(first.getName() + " and " + second.getName() + " are equals as Hufflepuff");
+        }
+    }
+    public static void compareRavenclawStudents(Hogwarts first, Hogwarts second) {
+        int firstSum = ((Ravenclaw) first).getCreativity() + ((Ravenclaw) first).getSmart() + ((Ravenclaw) first).getWise() + ((Ravenclaw) first).getWitty();
+        int secondSum = ((Ravenclaw) second).getCreativity() + ((Ravenclaw) second).getSmart() + ((Ravenclaw) second).getWise() + ((Ravenclaw) second).getWitty();
+
+        if (firstSum > secondSum) {
+            System.out.println(first.getName() + " is a better Ravenclaw than " + second.getName());
+        } else if (secondSum > firstSum) {
+            System.out.println(second.getName() + " is better Ravenclaw than " + first.getName());
+        } else {
+            System.out.println(first.getName() + " and " + second.getName() + " are equals as Ravenclaw");
+        }
+    }
+    public static void compareHogwartsStudents(Hogwarts first, Hogwarts second) {
+        int firstSum = first.getDistanceOfTransgression() + first.getPowerOfMagic();
+        int secondSum = second.getDistanceOfTransgression() + second.getPowerOfMagic();
+
+        if (firstSum > secondSum) {
+            System.out.println(first.getName() + " is a better Hogwarts than " + second.getName());
+        } else if (secondSum > firstSum) {
+            System.out.println(second.getName() + " is better Hogwarts than " + first.getName());
+        } else {
+            System.out.println(first.getName() + " and " + second.getName() + " are equals as Hogwarts");
+        }
+    }
+
 
 }
